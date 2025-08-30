b21b81612ba007200f6dacc44d7929d4eccc7f5d
Zach <dnfjddk2@gmail.com>
2025-08-30 12:17:20 +0900

    Add main functionality and tools for image processing
    - Implement main.py with hotkey functionality for interacting with Grand Theft Auto V.
    - Add requirements.txt for project dependencies.
    - Create blackandwhite-converter.py for converting images to black and white.
    - Develop threshold-trackbar.py for dynamic thresholding of images using a trackbar.

---
+++ Lester-Ver2.0/main.py
@@ -0,0 +1,75 @@
+import sys
+import time
+import pynput
+import ctypes
+from threading import Thread
+from win32gui import FindWindow, GetWindowRect
+from hacks import casinofingerprint, casinokeypad, cayofingerprint, cayovoltage
+
+def print_banner():
+    print('''
+
+██╗░░░░░███████╗░██████╗████████╗███████╗██████╗░  ██╗░░░██╗███████╗██████╗░  ██████╗░░░░░█████╗░
+██║░░░░░██╔════╝██╔════╝╚══██╔══╝██╔════╝██╔══██╗  ██║░░░██║██╔════╝██╔══██╗  ╚════██╗░░░██╔══██╗
+██║░░░░░█████╗░░╚█████╗░░░░██║░░░█████╗░░██████╔╝  ╚██╗░██╔╝█████╗░░██████╔╝  ░░███╔═╝░░░██║░░██║
+██║░░░░░██╔══╝░░░╚═══██╗░░░██║░░░██╔══╝░░██╔══██╗  ░╚████╔╝░██╔══╝░░██╔══██╗  ██╔══╝░░░░░██║░░██║
+███████╗███████╗██████╔╝░░░██║░░░███████╗██║░░██║  ░░╚██╔╝░░███████╗██║░░██║  ███████╗██╗╚█████╔╝
+╚══════╝╚══════╝╚═════╝░░░░╚═╝░░░╚══════╝╚═╝░░╚═╝  ░░░╚═╝░░░╚══════╝╚═╝░░╚═╝  ╚══════╝╚═╝░╚════╝░
+                                                                                          ''')
+
+def print_credits():
+    print('''
+Made by JUSTDIE
+Special thanks to RedHeadEmile
+    ''')
+    
+def check_window():
+    print('[*] Searching Grand Theft Auto V...')
+
+    while True:
+        hwnd = FindWindow(None, "Grand Theft Auto V")
+        
+        if hwnd:
+            print('[*] Grand Theft Auto V Detected!')
+            print('')
+            print('=============================================')
+            return GetWindowRect(hwnd)
+        
+        time.sleep(1)
+
+def casino_fingerprint(bbox):
+    thread = Thread(target=casinofingerprint.main, args=(bbox,))
+    thread.start()
+
+def casino_keypad(bbox):
+    thread = Thread(target=casinokeypad.main, args=(bbox,))
+    thread.start()
+
+def cayo_fingerprint(bbox):
+    thread = Thread(target=cayofingerprint.main, args=(bbox,))
+    thread.start()
+
+def cayo_voltage(bbox):
+    thread = Thread(target=cayovoltage.main, args=(bbox,))
+    thread.start()
+
+def shutdown():
+    sys.exit()
+
+def main():
+    print_banner()
+    print_credits()
+
+    bbox = check_window()
+    if bbox:
+        with pynput.keyboard.GlobalHotKeys({
+                '<F4>': shutdown,
+                '<F5>': lambda: casino_fingerprint(bbox),
+                '<F6>': lambda: casino_keypad(bbox),
+                '<F7>': lambda: cayo_fingerprint(bbox),
+                '<F8>': lambda: cayo_voltage(bbox)}) as h:
+            h.join()
+
+if __name__ == "__main__":
+    ctypes.windll.user32.SetProcessDPIAware()
+    main()
